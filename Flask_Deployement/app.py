9afaf23cd6c9cb6326f8c050a38284a5b4845ef1
Mayank <mayankgarg785mi@gmail.com>
2025-07-11 16:39:09 +0530

    basic flask app ready

--- Flask_Deployement/app.py
+++ Flask_Deployement/app.py
@@ -19,14 +19,34 @@ def index():
 @app.route("/predict_pro", methods=["POST"])
 def predict_pro():
     form = request.form.to_dict()
-    result = predict_main(**form)
-    return f"Predicted Popularity Score (Pro): {result}"
-
+    result = predict_main(
+        form['genre'],
+        form['key'],
+        form['mode'],
+        form['time_signature'],
+        float(form['acousticness']),
+        float(form['danceability']),
+        int(form['duration_ms']),
+        float(form['energy']),
+        float(form['instrumentalness']),
+        float(form['liveness']),
+        float(form['loudness']),
+        float(form['speechiness']),
+        float(form['tempo']),
+        float(form['valence'])
+    )
+    return render_template("result.html", prediction = result, user = 'Professional')
+    
 @app.route("/predict_common", methods=["POST"])
 def predict_common():
     form = request.form.to_dict()
-    result = predict_general(**form)
-    return f"Predicted Popularity Score (Common): {result}"
+    result = predict_general(
+        form['genre'],
+        form['mood'],
+        form['tempo_category'],
+        form['duration_category']
+    )
+    return render_template("result.html", prediction = result, user = 'Common User')
 
 if __name__ == "__main__":
     app.run(debug=True)
